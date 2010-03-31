34c8ef7f42b84ad309c29ce5f47024d166dd05ce
edo888 <edo888@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-31 17:34:12 +0000

    git-svn-id: https://plugins.svn.wordpress.org/gtranslate/trunk@223666 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gtranslate.php
+++ gtranslate.php
@@ -26,3 +26,62 @@ Author URI: http://edo.webmaster.am
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+#error_reporting(E_ALL);
+add_action('widgets_init', array('GTranslate', 'register'));
+register_activation_hook(__FILE__, array('GTranslate', 'activate'));
+register_deactivation_hook(__FILE__, array('GTranslate', 'deactivate'));
+
+class GTranslate extends WP_Widget {
+    function activate() {
+        $data = array(
+            'gtranslate_title' => 'GTranslate',
+            'gtranslate_method' => 'on-fly',
+            'gtranslate_pro' => 0,
+            'gtranslate_look' => 'both',
+            'gtranslate_flag_size' => 16,
+            'gtranslate_new_window' => 0,
+            'gtranslate_main_lang' => 'en',
+        );
+
+        if(get_option('GTranslate'))
+            update_option('GTranslate', $data);
+        else
+            add_option('GTranslate', $data);
+    }
+
+    function deactivate() {
+        delete_option('GTranslate');
+    }
+
+    function control() {
+        $data = get_option('GTranslate');
+        // -- TODO -- Generate the options form
+        ?>
+        <p><label>Title: <input name="gtranslate_title" type="text" class="widefat" value="<?php echo $data['gtranslate_title']; ?>"/></label></p>
+        <p><input type="checkbox" value="1" name="gtranslate_pro" id="gtranslate_pro" <?php if((int)$data['gtranslate_pro']) echo 'checked="checked"'; ?>/><label for="gtranslate_pro">Operate with GTranslate Pro?</label></p>
+        <p><input type="checkbox" value="1" name="gtranslate_new_window" id="gtranslate_new_window" <?php if((int)$data['gtranslate_new_window']) echo 'checked="checked"'; ?>/><label for="gtranslate_new_window">Open in a new window?</label></p>
+        <?php
+        if (isset($_POST['gtranslate_title'])){
+            $data['gtranslate_title'] = attribute_escape($_POST['gtranslate_title']);
+            $data['gtranslate_pro'] = attribute_escape($_POST['gtranslate_pro']);
+            $data['gtranslate_new_window'] = attribute_escape($_POST['gtranslate_new_window']);
+            update_option('GTranslate', $data);
+        }
+    }
+
+    function widget($args) {
+        $data = get_option('GTranslate');
+
+        echo $args['before_widget'];
+        echo $args['before_title'] . $data['gtranslate_title'] . $args['after_title'];
+        echo 'I am your widget'; // -- TODO -- display the language selector
+        echo $args['after_widget'];
+    }
+
+    function register() {
+        wp_register_sidebar_widget('gtranslate', 'GTranslate', array('GTranslate', 'widget'), array('description' => __('Google Automatic Translations')));
+        wp_register_widget_control('gtranslate', 'GTranslate', array('GTranslate', 'control'));
+        //register_sidebar_widget('GTranslate', array('GTranslate', 'widget'));
+        //register_widget_control('GTranslate', array('GTranslate', 'control'));
+    }
+}
